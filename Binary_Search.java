fe45b0727d630240f6c1a7df59390604b56d95ed
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2022-03-15 11:46:14 +0530

    Create Binary_Search.java

---
+++ Binary_Search.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Binary_Search {
+    int binarySearch(int[] arr, int l, int r, int x)
+    {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        Binary_Search ob = new Binary_Search();
+        Scanner S=new Scanner(System.in);
+        int n=S.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i< arr.length;i++){
+            arr[i]=S.nextInt();
+        }
+        int x=S.nextInt();
+        int res=ob.binarySearch(arr,0,n-1,x);
+        if(res==-1){
+            System.out.println("Element found"+" ");
+        }
+        else{
+            System.out.println("Element Not Found"+" ");
+        }
+
+    }
+}
+
